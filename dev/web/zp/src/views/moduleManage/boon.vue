01f7cc6fa54f59f660849f63282af84d5fb03111
unknown <272903696@qq.com>
2019-12-28 15:54:27 +0800

    '陈晓彤12.28moduleManage‘

--- dev/web/zp/src/views/moduleManage/boon.vue
+++ dev/web/zp/src/views/moduleManage/boon.vue
@@ -1,3 +1,22 @@
+<!--
+ @Author:Ivan
+ @Date:2019-12-28 15:22:56
+ @LastModifiedBy:Ivan
+ @Last Modified time:2019-12-28 15:22:56
+-->
+
+ @Last Modified time:2019-12-28 15:22:11
+-->
+
+ @Last Modified time:2019-12-26 22:15:55
+-->
+
+
+
+
+
+
+
 /*
  * @Author: liuyr 
  * 福利管理页面
@@ -6,19 +25,254 @@
  * @Last Modified time: 2019-12-23 21:07:41
  */
 <template>
-  <div id="moduleBoon">福利管理页面</div>
+  <div id="moduleBoon">
+    <!-- 下拉框 -->
+    <div class="selectDiv">
+      <el-select v-model="selectWelfare" clearable placeholder="全部" @change='selectChange'>
+        <el-option
+          v-for="item in welfareName"
+          :key="item"
+          :label="item"
+          :value="item">
+        </el-option>
+      </el-select>
+    </div>
+    <!-- 表格 -->
+    <div>
+      <el-table
+        ref="multipleTable"
+        :data="welfList"
+        tooltip-effect="dark"
+        style="width: 100%"
+        @selection-change="selectionChange">
+        <el-table-column
+          type="selection"
+          width="55">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="福利名称">
+        </el-table-column>
+        <el-table-column
+            id="divfru"
+            prop="status"
+            label="状态">
+            <template slot-scope="scope">
+          <div v-if="scope.row.status=='冻结中'" class="frozen" >
+            {{scope.row.status}}
+          </div>
+          <div v-else class="used"  >
+            {{scope.row.status}}
+          </div>
+        </template>
+        </el-table-column>
+        
+        <el-table-column
+        fixed="right"
+        label="操作"
+        width="100">
+        <template slot-scope="scope">
+          <div  v-if="scope.row.status=='冻结中'">
+             <el-button @click="recoverClick(scope.row)" type="success" size="mini">恢复</el-button>
+          </div>
+          <div  v-else>
+            <el-button @click="freezeClick(scope.row)" type="warning" size="mini">冻结</el-button>
+          </div>
+        </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <!-- 分页 -->
+    <div class="delandpage">
+      <div class="btndiv" >
+          <el-button type="danger" plain size="mini" @click="toBatchDelete()">批量删除</el-button>
+      </div>
+      <div class="pagediv">
+        <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="welfareselectData.length"
+        :current-page='startpage' 
+        @current-change="pagechange"
+        >
+        </el-pagination>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { findAllWelfare, findByIdWelfare,saveOrUpdateWelfare,deleteWelfareById} from "@/api/welfare.js";
+import config from '@/utils/config.js'
 export default {
   data() {
-    return {};
+    return {
+      //福利接口返回的所有总数据
+      welfareData:[],
+      //所有福利名称
+      welfareName:[],
+      //选中的福利
+      selectWelfare:'',
+      //获取的当前页数
+      startpage:1,
+      divstatus:'divfru',
+      welfareselectData:[],
+      //批量删除的ids
+      ids:[],
+
+    };
   },
-  computed: {},
-  methods: {},
-  created() {},
+  computed: {
+    welfList(){
+      let allwelfData=[...this.welfareselectData];
+      let page=this.startpage;//读当前页数
+      let setpage=10;//10行一页
+      return allwelfData.slice((page-1)*setpage,page*setpage)
+      // 计算属性的函数内记得写返回结果（返回结果要给回属性）
+    }
+  },
+  methods: {
+    // 查询所有福利信息
+    getallWelfare(){
+      findAllWelfare()
+      .then((res)=>{
+        this.welfareData=[...res.data];
+        this.welfareselectData=[...res.data];
+        let temp= res.data.map((item)=>{
+          return item.status;
+        });
+        this.welfareName=[...new Set(temp)];
+      })
+      .catch((error)=>{config.errorMsg(this, "查找错误")});
+    },
+    //恢复
+    recoverClick(val){
+      let id=val.id;
+      //console.log(id);
+      saveOrUpdateWelfare({id,status:'使用中',name:val.name})
+      .then((res)=>{
+        config.successMsg(this,'状态切换成功');
+        setTimeout(()=>{this.selectChange(this.selectWelfare);},50)
+        this.getallWelfare();
+      })
+      .catch((error)=>{config.errorMsg(this, "状态切换失败")});
+    },
+    //冻结
+    freezeClick(val){
+      //console.log(val);
+      //console.log(2);
+      let id=val.id;
+      //console.log(id);
+      saveOrUpdateWelfare({id,status:'冻结中',name:val.name})
+      .then((res)=>{
+        config.successMsg(this,'状态切换成功');
+        setTimeout(()=>{this.selectChange(this.selectWelfare);},50)
+        this.getallWelfare();
+        //this.selectChange(this.selectWelfare);
+      })
+      .catch((error)=>{config.errorMsg(this, "状态切换失败")});
+      
+      
+    },
+    //获取当前所选页数
+    pagechange(val){
+      this.startpage=val;
+    },
+    //下拉框
+    selectChange(val){
+      
+      if(val!=""){
+        let temp= this.welfareData.filter((item)=>{
+            return item.status==val;
+        });
+        //setTimeout(()=>{this.welfareData=[...temp]},2000);
+        this.startpage=1;
+        this.welfareselectData=temp;
+        
+      }else{
+        this.getallWelfare();
+      }
+    },
+    //复选框事件处理
+    selectionChange(val){
+      //提取id
+      let ids=val.map((item)=>{
+        return item.id;
+      })
+      this.ids=ids;
+    },
+    //多选删除
+    toBatchDelete(){
+      //获取被选中的数据 this.ids
+    let ids=this.ids;
+    if(ids.length>0){
+      this.$alert('是否删除？', '提示', {
+          confirmButtonText: '确定',
+          callback: action => {
+            if(action=='confirm'){
+              let result=[];
+              ids.forEach((id)=>{
+                 deleteWelfareById({id})
+                 .then((res)=>{
+                  result.push(res.status);
+                 })
+                 .catch((error)=>{
+                   result.push(500);
+                   //config.errorMsg(this, "查找错误");
+                   })
+              })
+              setTimeout(()=>{
+                //console.log(result);
+                let resu =result.every((item)=>{return item===200})
+                if(resu){
+                  config.successMsg(this,'批量成功');
+                }else{
+                  config.errorMsg(this, "批量删除失败");
+                }
+                this.getallWelfare();
+                this.selectWelfare='';
+              },2000)
+              //every用于遍历判断是不是全部都符合return的条件
+
+            }
+
+          }
+        });
+    }else{
+      this.$message({
+          message: '请选择要删除的对象',
+          type: 'warning'
+        });
+    }
+  },
+  },
+  created() {
+    this.getallWelfare();
+  },
   mounted() {}
 };
 </script>
-<style scoped>
+<style lang='scss' scoped>
+#divfru{
+  color: white;
+}
+.frozen{
+  color:orange;
+}
+.used{
+  color: forestgreen;
+}
+.delandpage{
+    overflow: hidden;
+    margin-top: 10px;
+    .btndiv{
+      float: left;
+    }
+    .pagediv{
+      float: right;
+    }
+  }
+.selectDiv{
+  margin-bottom: 20px;
+}
 </style>
--- dev/web/zp/src/views/moduleManage/position.vue
+++ dev/web/zp/src/views/moduleManage/position.vue
@@ -1,24 +1,285 @@
-/*
- * @Author: liuyr 
- * 职位管理页面
- * @Date: 2019-12-23 17:11:53 
- * @Last Modified by: liuyr
- * @Last Modified time: 2019-12-23 21:08:24
- */
+
+<!--
+ @Author:Ivan
+ @Date:2019-12-28 15:21:43
+ @LastModifiedBy:Ivan
+ @Last Modified time:2019-12-28 15:21:43
+-->
+
+ @Last Modified time:2019-12-28 15:21:34
+-->
+
 <template>
-  <div id="modulePosition">职位管理页面</div>
+  <div id="modulePosition">
+    <el-button id="add" size='small' type="warning" @click="clickadd()">添加职位类型</el-button>
+
+    <!-- 添加div -->
+    <div v-if="outsidediv==true">
+      <el-card class="box-card">
+      <div slot="header" class="clearfix">
+
+        <div class="inputset">
+          <div id="addinput"><el-input class="input50px"  v-model="inputjobtype" placeholder="请输入职位类型" id="provinceinput" size='mini'></el-input></div>
+          <div class="inputset"><el-button size='mini' @click="toaddJobType()">添加</el-button></div>
+        </div>
+      </div>
+
+      </el-card>
+    </div>
+  <!-- 循环生成当前已有卡片{{allJobType}} -->
+    <div id="card" v-for="(item,index) in allJobType" :key="index">
+      
+       <el-card class="box-card">
+        <div slot="header" class="clearfix">
+        <span>{{item.name}}</span>
+        </div>
+        <div v-for="(it,ind) in item.jobs" :key="ind" class="text item indiv">
+        {{it}}
+        </div>
+        
+        <div class="indiv">
+          <div class="inputset" v-if="item.outadd"><el-button @click="addJob(item)" type="text" size="small">添加</el-button></div>
+          <div v-if="item.add">
+            <div id="addinput"><el-input v-model="inputjob"  placeholder="请输入职位" size='mini' id="jobinput"></el-input></div>
+            <div class="inputset"><el-button  type="primary" size='mini' @click="saveJob(item,item.name)">添加</el-button></div>
+          </div>
+        </div>
+      </el-card>
+      </div>
+  </div>
+  
 </template>
 
 <script>
+import {findByStatusJobs,saveOrUpdateJobs } from "@/api/job.js";
+import { findAlljobType,saveOrUpdatejobType} from "@/api/job_type.js";
+import config from '@/utils/config.js'
+axios.defaults.baseURL="http://127.0.0.1:8899";
+import axios from "axios";
+// 设置基础路径
+import qs from 'qs';
 export default {
   data() {
-    return {};
+    return {
+      allJobType:[],
+      citylist:[],
+      inputjob:'',
+      inputjobtype:'',
+      outsidediv:false,
+      jobtypeagain:false,
+      jobagain:false,
+
+    };
   },
   computed: {},
-  methods: {},
-  created() {},
+  methods: {
+    //  getSt(){
+    //    axios.get('/Jobs/findByStatus',{params:{staus:"互联网"}}).then((res)=>{
+    //         console.log(res);
+            
+    //       }).catch((error)=>{console.log(error);});
+    //  }
+    // ,
+    //获取全部职业信息
+    getjobType(){
+      findAlljobType()
+      .then((res)=>{
+        let temp=res.data;
+        // console.log(temp);
+        
+        temp.forEach((item,index)=>{
+          findByStatusJobs({staus:item.name}).then((res)=>{
+            // console.log(res);
+            
+            // item.jobs=[...new Set(res.data)];
+            
+            // console.log(res.data);
+            let t= res.data.map((item)=>{
+              return item.name;
+            })
+            // console.log(t);
+            // item.jobs=t;
+            item.jobs=[...new Set(t)];
+            item.add=false;
+            item.outadd=true;
+
+            
+          }).catch((error)=>{console.log(error);});
+
+        });
+        setTimeout(()=>{
+          // temp= temp.map((item)=>{
+          //   
+          //   return item;
+          // });
+          // console.log(temp);
+          
+          
+          this.allJobType=[...temp];},1000);
+        
+      })
+      .catch((error)=>{console.log(error);});
+      
+    },
+    //显示添加职业框
+    addJob(item){
+      item.add=true;
+      item.outadd=false;
+      return item;
+    },
+    //新增职业
+    saveJob(item,inf){
+      item.add=false;
+      item.outadd=true;
+      // console.log(item);
+       item.jobs.forEach((item,index)=>{
+        //  console.log(item.name);
+        // console.log(item.name);
+        console.log(item);
+        if (item==this.inputjob) {
+            this.jobagain=true;
+            console.log(item);
+            
+          };
+      });
+      if(this.inputjob!=''){
+        if(this.jobagain==false){
+          saveOrUpdateJobs({name:this.inputjob,status:inf})
+          .then((res)=>{
+            this.getjobType();
+            this.inputjob='';
+            config.successMsg(this, "新增职位成功")
+          })
+          .catch((error)=>{console.log(error);
+        this.jobagain=false;
+        this.inputjob='';});
+        }else{
+          config.errorMsg(this, "请勿重复添加职位");
+          this.jobagain=false;
+          this.inputjob='';
+        }
+      }else{
+        config.errorMsg(this, "职业类别不能为空,新增职业类别失败");
+        this.jobagain=false;
+        this.inputjob='';
+      }
+      return item;
+    },
+    //显示添加职位类型框
+    clickadd(){
+      this.outsidediv=true;
+    },
+    // outsaveJob(){
+    //   this.outaddstuta=true;
+    //   this.addstuta=false;
+    //   if(this.inputcity!=''){
+    //     saveOrUpdateCity({provinceId:id,name:this.inputcity})
+    //   .then((res)=>{
+    //     this.getjobType();
+    //     this.inputcity='';
+    //   })
+    //   .catch((error)=>{console.log(error);});
+    //   }
+    //   return item;
+    // },
+    //添加新增职业类别
+    toaddJobType(){
+      //console.log(this.inputjobtype);
+      this.allJobType.forEach((item,index)=>{
+        if(item.name==this.inputjobtype){
+          this.jobtypeagain=true;
+        }
+      });
+
+      if(this.inputjobtype!=''){
+        if(!this.jobtypeagain){
+          saveOrUpdatejobType({name:this.inputjobtype})
+        .then((res)=>{
+          console.log(res);
+          config.successMsg(this, "新增职业类别成功")
+          this.getjobType();
+          this.inputjobtype='';
+          this.outsidediv=false;
+        })
+        .catch((error)=>{config.errorMsg(this, "新增错误")
+        console.log(error);
+        });
+        }else{
+          config.errorMsg(this, "该行业已存在，请勿重复添加");
+          this.inputjobtype='';
+           this.jobtypeagain=false;
+        }
+      }else{
+        config.errorMsg(this, "职业类别不能为空,新增职业类别失败");
+        this.outsidediv=false;
+      }
+
+      
+
+    },
+    
+
+  },
+  created() {
+    this.getjobType();
+    // this.getSt();
+  },
   mounted() {}
 };
 </script>
 <style scoped>
+   .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 1000px;
+    margin: 25px 80px;
+  }
+
+  #add{
+    position: relative;
+    left: 1000px;
+    
+  }
+  .indiv{
+    display: inline-block;
+    margin: 15px;
+
+  }
+  #jobinput{
+    width: 50px;
+  }
+  .inputset{
+    display: inline-block;
+  }
+  #addinput{
+    
+    display: inline-block;
+  }
+  /* #provinceinput{
+    display: inline-block;
+    width: 50px;
+  } */
+
+  /* #id{
+    div{
+      p{
+
+      }
+    }
+  } */
 </style>
